4fd5fd95631cacb7bcc734d6a7487f720b24ce5e
Godknows Christians Nyamatendedza <godycnyama@gmail.com>
2023-07-28 09:29:30 +0200

    refactored the elevator control logic

--- ElevatorSimulatorDomain/Elevator.cs
+++ ElevatorSimulatorDomain/Elevator.cs
@@ -95,6 +95,10 @@
 
         public void PickUpPassengers()
         {
+            if (_elevatorDirection == 0)
+            {
+                Console.WriteLine("Elevator " + _elevatorId + " is not moving");
+            }
             int _numberOfPassengersToPickUp = _elevatorMaximumCapacity - _elevatorCurrentCapacity;
             int _numberOfPassengersWaiting = _totalCurrentRequests.Count(x => x.OriginFloor == _currentFloor);
             int _numberOfPassengersPickedUp = 0;
@@ -107,10 +111,12 @@
 
             if (_elevatorDirection == 1) // if elevator is moving up
             {
+                Console.WriteLine("Elevator " + _elevatorId + " is on floor " + _currentFloor + " moving up");
                 _potentialRequests = _totalCurrentRequests.Where(x => x.OriginFloor == _currentFloor && x.DestinationFloor > _currentFloor).ToList();
             }
             else if (_elevatorDirection == -1) // if elevator is moving down
             {
+                Console.WriteLine("Elevator " + _elevatorId + " is on floor " + _currentFloor + " moving down");
                 _potentialRequests = _totalCurrentRequests.Where(x => x.OriginFloor == _currentFloor && x.DestinationFloor < _currentFloor).ToList();
             } 
             else if (_elevatorDirection == 0) // if elevator is not moving
@@ -119,16 +125,19 @@
                 {
                     _potentialRequests = _requestsUp;
                     _elevatorDirection = 1;
+                    Console.WriteLine("Elevator " + _elevatorId + " is on floor " + _currentFloor + " moving up");
                 }
                 else if (_requestsUp.Count < _requestsDown.Count)
                 {
                     _potentialRequests = _requestsDown;
                     _elevatorDirection = -1;
+                    Console.WriteLine("Elevator " + _elevatorId + " is on floor " + _currentFloor + " moving down");
                 }
                 else if (_requestsUp.Count == _requestsDown.Count)
                 {
                     _potentialRequests = _requestsUp;
                     _elevatorDirection = 1;
+                    Console.WriteLine("Elevator " + _elevatorId + " is on floor " + _currentFloor + " moving up");
                 }
             }
 
@@ -147,7 +156,10 @@
                 }
             }
 
-            Console.WriteLine("Elevator " + _elevatorId + " picked up " + _numberOfPassengersPickedUp + " passengers from floor " + _currentFloor);
+            if (_numberOfPassengersPickedUp > 0)
+            {
+                Console.WriteLine("Elevator " + _elevatorId + " picked up " + _numberOfPassengersPickedUp + " passengers from floor " + _currentFloor);
+            }
         }
 
         public void DropOffPassengers()
@@ -155,7 +167,10 @@
             List<ElevatorRequest> _completedRequests = _thisElevatorRequests.Where(x => x.DestinationFloor == _currentFloor).ToList();
             _elevatorCurrentCapacity -= _completedRequests.Count;
             _thisElevatorRequests.RemoveAll(x => x.DestinationFloor == _currentFloor);
-            Console.WriteLine("Elevator " + _elevatorId + " dropped off " + _completedRequests.Count + " passengers at floor " + _currentFloor);
+            if (_completedRequests.Count > 0)
+            {
+                Console.WriteLine("Elevator " + _elevatorId + " dropped off " + _completedRequests.Count + " passengers at floor " + _currentFloor);
+            }
         }
 
         public void ExecuteMove()
@@ -170,19 +185,23 @@
                 CloseDoor();
                 if (_elevatorDirection == 1) //continue moving up
                 {
+                    Console.WriteLine("Elevator " + _elevatorId + " is on floor " + _currentFloor + " moving up");
                     MoveUp();
                 }
                 else if (_elevatorDirection == -1) //continue moving down
                 {
+                    Console.WriteLine("Elevator " + _elevatorId + " is on floor " + _currentFloor + " moving down");
                     MoveDown();
                 }
             }
             else if (_elevatorDirection == 1 && _elevatorCurrentCapacity == _elevatorMaximumCapacity) //if elevator is full continue moving up
             {
+                Console.WriteLine("Elevator " + _elevatorId + " is on floor " + _currentFloor + " moving up");
                 MoveUp();
             }
             else if (_elevatorDirection == -1 && _elevatorCurrentCapacity == _elevatorMaximumCapacity) //if elevator is full continue moving down
             {
+                Console.WriteLine("Elevator " + _elevatorId + " is on floor " + _currentFloor + " moving down");
                 MoveDown();
             }
         }
